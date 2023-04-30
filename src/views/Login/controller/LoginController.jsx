f6edb483c19b67a9fcf61f35caf8fdf7c42e5f9b
Sajidh Farook <sajidh@begood.solutions>
2023-04-30 14:43:50 +0530

    refactor: added comments

--- src/views/Login/controller/LoginController.jsx
+++ src/views/Login/controller/LoginController.jsx
@@ -2,6 +2,7 @@ import LoginView from "../view/LoginView.jsx";
 
 const LoginController = () => {
 
+    // Handle login function API call and validations
     const handleLoginSubmit = () => {
         console.log("Login Submit")
     }
