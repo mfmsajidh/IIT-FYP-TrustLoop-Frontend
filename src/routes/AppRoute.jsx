f3cf22468bc3203b0c333db5714aafc18d793834
Sajidh Farook <sajidh@begood.solutions>
2023-05-03 17:07:07 +0530

    Changed default router

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,15 +1,15 @@
-import { Route, Routes,Navigate} from 'react-router-dom'
-import {Home, Login, Register} from "../views/index.js";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Home, Login, Register } from "../views/index.js";
 const AppRoute = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/signIn" />} />
-            <Route exact path='/signIn' element={<Login/>} />
-            <Route exact path='/signUp' element={<Register/>} />
-            <Route exact path='/home' element={<Home/>} />
-            <Route path='*' element={<>Path not found</>} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route exact path="/signIn" element={<Login />} />
+      <Route exact path="/signUp" element={<Register />} />
+      <Route exact path="/home" element={<Home />} />
+      <Route path="*" element={<>Path not found</>} />
+    </Routes>
+  );
+};
 
-export default AppRoute
+export default AppRoute;
